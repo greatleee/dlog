c7f6e0877c7277efc74736f25c482fb720d00538
greatleee <cd.lee.dev@gmail.com>
2021-12-29 00:24:13 +0900

    feat:Calendar.tsx 스타일 설정, 소주 아이콘 추가

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,24 +1,36 @@
-import useCalendar from '@veccu/react-calendar';
 import { format } from 'date-fns'
 import locale from 'date-fns/locale/ko'
+import useCalendar from '@veccu/react-calendar';
+import styles from './Calendar.module.css';
+import sojuBasicImage from '../assets/images/soju_basic.svg'
 
 const Calendar = () => {
-  const { headers, body, view } = useCalendar()
+  const { headers, body } = useCalendar()
 
   return (
-    <table>
+    <table className={ styles.table }>
       <thead>
         <tr>
-          {headers.weekDays.map(({key, value}) => {
-            return <th key={ key }>{ format(value, 'E', { locale }) }</th>
-          })}
+          {headers.weekDays.map(({key, value}) => (
+            <th key={ key }>
+              { format(value, 'E', { locale }) }
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {body.value.map(({ key, value: days }) => (
           <tr key={ key }>
             {days.map(({ key, date }) => (
-              <td key={ key }>{ date }</td>
+              <td key={ key }>
+                <div className={ styles.item }>
+                  <div>{ date }</div>
+
+                  <figure>
+                    <img src={sojuBasicImage} alt="" />
+                  </figure>
+                </div>
+              </td>
             ))}
           </tr>
         ))}
