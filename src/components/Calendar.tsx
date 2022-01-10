f3370c7eca561521a441b98071555e4b1cf4ab8b
greatleee <cd.lee.dev@gmail.com>
2022-01-10 09:38:51 +0900

    feat: 달력 view 기능 완성
    - 오늘 날짜에 highlight
    - 이번 달이 아닌 날은 캐릭터 이미지와 이미지 마스킹 모두 뜨지 않도록 설정

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns'
 import locale from 'date-fns/locale/ko'
 import useCalendar from '@veccu/react-calendar';
+import { IonContent } from '@ionic/react';
 import styles from './Calendar.module.css';
 import sojuBasicImage from '../assets/images/soju_basic.svg'
 
@@ -8,34 +9,38 @@ const Calendar = () => {
   const { headers, body } = useCalendar()
 
   return (
-    <table className={ styles.table }>
-      <thead>
-        <tr>
-          {headers.weekDays.map(({key, value}) => (
-            <th key={ key }>
-              { format(value, 'E', { locale }) }
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {body.value.map(({ key, value: days }) => (
-          <tr key={ key }>
-            {days.map(({ key, date }) => (
-              <td key={ key }>
-                <div className={ styles.item }>
-                  <div>{ date }</div>
-
-                  <figure>
-                    <img src={sojuBasicImage} alt="" />
-                  </figure>
-                </div>
-              </td>
+    <IonContent>
+      <table className={ styles.table }>
+        <thead>
+          <tr>
+            {headers.weekDays.map(({key, value}) => (
+              <th key={ key }>
+                { format(value, 'E', { locale }) }
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {body.value.map(({ key, value: days }) => (
+            <tr key={ key }>
+              {days.map(({ key, date, value, isCurrentDate, isCurrentMonth }) => (
+                <td key={ key }>
+                  { isCurrentMonth && <div className={ styles.item }>
+                    <div className={ isCurrentDate ? styles.today : '' }>
+                      <div>{ date }</div>
+                    </div>
+
+                    <figure>
+                      <img src={sojuBasicImage} alt=""/>
+                    </figure>
+                  </div> }
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </IonContent>
   )
 }
 
